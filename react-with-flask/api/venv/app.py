dacb20833f297c378baf433abcc6d6959f753886
Jahzeel Reid <reidJahzeel@gmail.com>
2025-08-14 22:34:14 -0400

    hate

--- react-with-flask/api/venv/app.py
+++ react-with-flask/api/venv/app.py
@@ -1,5 +1,7 @@
 import time
-from flask import Flask
+from flask import Flask, request, jsonify
+import requests
+from requests.auth import HTTPBasicAuth
 
 app = Flask(__name__)
 
@@ -7,3 +9,35 @@ app = Flask(__name__)
 @app.route("/api/time")
 def get_current_time():
     return {"time": time.time()}
+
+
+@app.route("/api/user", methods=["POST"])
+def pquery():
+    auth = request.get_json()
+    print(auth)
+    acc_id = auth["auth"]
+    url = "https://api.teller.io/accounts"
+    auth = HTTPBasicAuth(
+        auth["auth"]["accessToken"], ""
+    )  # Teller uses access_token as username
+
+    account_info = requests.get(url, auth=auth)
+    print(account_info.json())
+    acc_id = account_info
+
+    if account_info.status_code != 200:
+        return jsonify({"error": account_info.text}), account_info.status_code
+
+    for i in range(len(account_info)):
+        print("Account value  ", i)
+        url = account_info[i]["balances"]
+        bal_info = requests.get(url, auth=auth)
+
+    return {"done": "done"}
+
+
+# https://api.teller.io/accounts/acc_phaouss0gjg4d1rtu8000/balances
+# https://api.teller.io/accounts/acc_phaouss1gjg4d1rtu8000/balances
+
+
+# kick the can without sitting with the mistakes and taking accountability
--- react-with-flask/src/App.tsx
+++ react-with-flask/src/App.tsx
@@ -1,10 +1,63 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from "axios";
+
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+// import SimpleTellerConnect from './Tellerbasic'
+// import TellerConnectClass from './Tellercomp'
+// import { TellerConnect } from 'teller-connect-react';
+import { useTellerConnect } from 'teller-connect-react';
 
 function App() {
   const [count, setCount] = useState(0)
+  const [currentTime, setCurrentTime] = useState(0);
+  const [token, setToken] = useState();
+  const app_id = "app_ph83hsn3hg9ukkife2000";
+  
+
+  const { open, ready } = useTellerConnect({
+    applicationId: app_id,
+    environment: "sandbox",
+    onSuccess: (authorization) => {
+      // Save your access token here
+      console.log("Sandbox Access Token:", authorization.accessToken);
+      getclientlist(authorization)
+      // send to api
+      // token = authorization.accessToken;
+      // setToken(authorization.accessToken)
+
+    },
+  });
+
+  useEffect(() => {
+    fetch('/api/time').then(res => res.json()).then(data => {
+      setCurrentTime(data.time);
+    });
+  }, []);
+
+  function getclientlist(accesstoken: any) {
+    axios({
+      method: "POST",
+      url: `/api/user`,
+      data: {
+        auth: accesstoken,
+      },
+    })
+      .then((response) => {
+        setToken(response.data);
+      })
+      .catch((error) => {
+        if (error.response) {
+          console.log(error.response);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        }
+      });
+  }
+
+
+
 
   return (
     <>
@@ -21,9 +74,18 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <p>The current time is {new Date(currentTime * 1000).toLocaleString()}.</p>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
+        {/* <SimpleTellerConnect/> */}
+        {/* <TellerConnectClass/> */}
+        <button onClick={() => open()} disabled={!ready}>
+          Connect a bank account
+        </button>
+        {/* <p>Access values: {token} </p> */}
+        
+
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
